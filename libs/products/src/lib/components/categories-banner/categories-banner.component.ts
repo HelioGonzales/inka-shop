2c22c72588a2060fe507a3bd9395542ea9930c0f
HelioGonzales <heliogonzalesr@gmail.com>
2022-08-22 21:44:18 +0200

    before adding the suscription in category banner

--- libs/products/src/lib/components/categories-banner/categories-banner.component.ts
+++ libs/products/src/lib/components/categories-banner/categories-banner.component.ts
@@ -1,28 +1,20 @@
-import { Subscription } from 'rxjs';
-import { CategoriesService } from '@inka-shop/products';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/category';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'products-categories-banner',
   templateUrl: './categories-banner.component.html',
   styles: [],
 })
-export class CategoriesBannerComponent implements OnInit, OnDestroy {
+export class CategoriesBannerComponent implements OnInit {
   categories: Category[] = [];
-  endSubs$!: Subscription;
 
-  constructor(private categoriesSvc: CategoriesService) {}
+  constructor(private caregoriesSvc: CategoriesService) {}
 
   ngOnInit(): void {
-    this.endSubs$ = this.categoriesSvc.getCategories().subscribe((res) => {
-      console.log(res);
-
-      this.categories = res;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.endSubs$.unsubscribe();
+    this.caregoriesSvc
+      .getCategories()
+      .subscribe((res) => (this.categories = res));
   }
 }
